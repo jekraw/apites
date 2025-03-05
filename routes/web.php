42e8fbadb4a00e38b16e9886cda21106a8439388
jeki <gorillaaaaaaaaa@gmail.com>
2025-03-06 02:06:47 +0700

    update1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get-user', [App\Http\Controllers\UserController::class, 'getUser']);
+Route::post('/add-user', [App\Http\Controllers\UserController::class, 'postUser']);
+Route::put('/update-user/{id}', [App\Http\Controllers\UserController::class, 'updateUser']);
+Route::delete('/delete-user/{id}', [App\Http\Controllers\UserController::class, 'deleteUser']);
+Route::post('login', [UserController::class, 'login']);
